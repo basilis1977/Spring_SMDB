55196d9550be96769ecebe25f0b94ad7e39f5d62
Vasilis Alchazidis <basilis.a@gmail.com>
2021-05-19 09:53:20 +0300

    Initial commit

---
+++ src/test/java/com/example/spring_smdb/SpringSmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_smdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSmdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
